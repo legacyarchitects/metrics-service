335f71c92538f2f3251014c522d3d52ad634bd3a
George Yoo <george@MacBook-Pro.local>
2024-02-01 08:50:56 -0800

    updates connection to dynamodb

--- game-tracker/app.js
+++ game-tracker/app.js
@@ -1,15 +1,40 @@
+const AWS = require('aws-sdk');
+require("dotenv").config();
 
 
-exports.lambdaHandler = async (event, context) => {
-    try {
-        return {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'hello world',
-            })
-        }
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
+exports.lambdaHandler = async (event) => {
+  let body;
+  let statusCode = 200;
+  const headers = {
+    "Content-Type": "application/json"
+  };
+  
+  try {
+      const dynamoDB = new AWS.DynamoDB.DocumentClient();
+      let requestJSON = JSON.parse(event.body);
+      await dynamoDB.put({
+          TableName: "game_markers",
+          Item: {
+              id: requestJSON.id,
+              game: "game",
+              first_name: requestJSON.first_name,
+              last_name: requestJSON.last_name,
+              lawyer_name: requestJSON.lawyer_name,
+              time_clicked: requestJSON.time_clicked,
+          }
+      }).promise();
+
+      body = `Item with id ${requestJSON.id} added.`;
+  } catch (err) {
+      statusCode = 400;
+      body = err.message;
+  } finally {
+      body = JSON.stringify(body);
+  }
+
+  return {
+      statusCode,
+      body,
+      headers
+  };
 };
