7680cc78c44e37af984015266f51329add7136f4
bensboto <bensboto@gmail.com>
2024-02-01 14:25:20 -0700

    adds 2 GET requests for fetching client data from dynamo

--- game-tracker/app.js
+++ game-tracker/app.js
@@ -1,7 +1,6 @@
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 require("dotenv").config();
-const { v4: uuidv4 } = require('uuid');
-
+const { v4: uuidv4 } = require("uuid");
 
 exports.lambdaHandler = async (event) => {
   let body;
@@ -9,36 +8,89 @@ exports.lambdaHandler = async (event) => {
   const headers = {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*", // Allows access from any origin
-    "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,Access-Control-Allow-Methods",
+    "Access-Control-Allow-Headers":
+      "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,Access-Control-Allow-Methods",
     "Access-Control-Allow-Methods": "OPTIONS,GET,PUT,POST,DELETE", // Adjust based on the methods your API supports
   };
-  
-  try {
+  if (event.httpMethod === "PUT") {
+    try {
       const dynamoDB = new AWS.DynamoDB.DocumentClient();
       let requestJSON = JSON.parse(event.body);
-      await dynamoDB.put({
+      await dynamoDB
+        .put({
           TableName: "game_markers",
           Item: {
-              game: uuidv4(),
-              client_id: requestJSON.client_id,
-              first_name: requestJSON.first_name,
-              last_name: requestJSON.last_name,
-              lawyer_name: requestJSON.lawyer_name,
-              time_clicked: requestJSON.time_clicked,
-          }
-      }).promise();
+            game: uuidv4(),
+            client_id: requestJSON.client_id,
+            first_name: requestJSON.first_name,
+            last_name: requestJSON.last_name,
+            lawyer_name: requestJSON.lawyer_name,
+            time_clicked: requestJSON.time_clicked,
+          },
+        })
+        .promise();
 
       body = `Item with id ${requestJSON.client_id} added.`;
-  } catch (err) {
+    } catch (err) {
       statusCode = 400;
       body = err.message;
-  } finally {
+    } finally {
       body = JSON.stringify(body);
-  }
+    }
 
-  return {
+    return {
       statusCode,
       body,
-      headers
-  };
+      headers,
+    };
+  } else if (
+    event.httpMethod === "GET" &&
+    event.resource === "/api/contacts/{id}"
+  ) {
+    try {
+      const dynamoDB = new AWS.DynamoDB.DocumentClient();
+      const id = event.pathParameters.id; // Extracting id from path parameters
+      const result = await dynamoDB
+        .get({
+          TableName: "game_markers" || process.env.DYNAMODB_TABLE_NAME, // Using environment variable for table name
+          Key: { game: id },
+        })
+        .promise();
+
+      body = result.Item ? result.Item : `Item with id ${id} not found.`;
+    } catch (err) {
+      statusCode = 400;
+      body = err.message;
+    } finally {
+      body = JSON.stringify(body);
+    }
+
+    return {
+      statusCode,
+      body,
+      headers,
+    };
+  } else if (event.httpMethod === "GET" && event.resource === "/api/contacts") {
+    try {
+      const dynamoDB = new AWS.DynamoDB.DocumentClient();
+      const result = await dynamoDB
+        .scan({
+          TableName: "game_markers" || process.env.DYNAMODB_TABLE_NAME, // Using environment variable for table name
+        })
+        .promise();
+
+      body = result.Items;
+    } catch (err) {
+      statusCode = 400;
+      body = err.message;
+    } finally {
+      body = JSON.stringify(body);
+    }
+
+    return {
+      statusCode,
+      body,
+      headers,
+    };
+  }
 };
